8693f7f9e9c93e98c17dde3e5265828161d1dc78
aftandilmmd <aftandil.mmd@gmail.com>
2022-08-23 11:18:11 +0300

    Move model created event logic to Service Class

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,31 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
+use App\Services\TenantService;
 use Spatie\Multitenancy\Models\Tenant as BaseTenant;
 
 class Tenant extends BaseTenant
 {
     protected static function booted(): void
     {
-        static::creating(static fn(Tenant $model) => $model->runCreateActions());
-    }
+        static::created(static function(Tenant $tenant) {
 
-    public function runCreateActions(): void
-    {
-        $this->createDatabase();
+            TenantService::createDatabase($tenant);
 
-        $this->migrate();
-    }
+            TenantService::migrate();
 
-    public function createDatabase(): void
-    {
-        DB::connection('tenant')->statement('CREATE DATABASE ' . $this->getDatabaseName());
-    }
-
-    public function migrate(): void
-    {
-        Artisan::call('tenants:artisan "migrate --database=tenant"');
+        });
     }
 }
