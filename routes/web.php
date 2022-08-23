87dda5f166182212a0d34980f57bbdc8b36abb9d
aftandilmmd <aftandil.mmd@gmail.com>
2022-08-23 15:33:28 +0300

    Add tenant check route

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::middleware('tenant')->group(function() {
+    Route::get('/tenant', static function () {
+
+        dd( \App\Models\Tenant::current());
+
+        dd(\App\Models\User::all());
+
+    });
+});
+
+
 
 Route::get('/create-tenant', static function () {
 
@@ -21,8 +20,8 @@ Route::get('/create-tenant', static function () {
 
     $tenant = new \App\Models\Tenant;
     $tenant->name = 'Caner';
-    $tenant->domain = 'salon_'. $random;
-    $tenant->database = 'tenant_salon_'.$random;
+    $tenant->domain = 'kozmoz';
+    $tenant->database = 'kozmoz';
     $tenant->save();
 
     return $tenant;
